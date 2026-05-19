76d8e0a4b95056b5c58eb666c27aecc325d4ec80
suciaisyahputri03-cloud <suciaisyahputri03@gmail.com>
2026-05-19 18:48:17 +0700

    menambahkan constructor pada kelas budi

--- solveMultiwaris.cpp
+++ solveMultiwaris.cpp
@@ -32,5 +32,10 @@ public:
 
 class budi : public pekerja, public pelajar {
 public:
-    
-}
+    budi(int pUmur) :
+        pekerja(pUmur),
+        pelajar(pUmur)
+    {
+        cout << "budi dibuat\n" << endl;
+    }
+};
